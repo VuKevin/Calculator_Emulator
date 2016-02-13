c1488cefe0b21a2f653eb442ca0318003a22a9e6
Kevin Vu <VuKevin@icloud.com>
2016-02-13 03:06:35 -0800

    Create UnbalancedParentheses.cpp

---
+++ UnbalancedParentheses.cpp
@@ -0,0 +1,8 @@
+#include "UnbalancedParentheses.h"
+
+namespace VuKevin
+{
+	UnbalancedParentheses::UnbalancedParentheses(const std::string & msg)
+		: std::logic_error("Unbalanced parentheses found.")
+	{}
+}
